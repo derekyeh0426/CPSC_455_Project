899eccd3e84c1257d63707351abdf496765c3b0c
hsinhao <c8e65e7d1a9f9b38a380d108d8ccf5b8c7a69e9e>
2021-06-11 22:57:38 -0700

    add logo to About.js

--- frontend/pfiniture/src/components/About.js
+++ frontend/pfiniture/src/components/About.js
@@ -1,5 +1,7 @@
 import { Form, Button, Row, Col, Container, FormGroup, Image, Modal, ModalBody, Nav } from "react-bootstrap";
 import React from 'react';
+import "./About.css"
+import image from '../assets/FF_12.png';
 
 class About extends React.Component {
     constructor(props) {
@@ -17,7 +19,7 @@ class About extends React.Component {
         this.setState({ show: false })
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <Button variant="light" className="button-modal" onClick={this.handleShow}>
@@ -27,9 +29,18 @@ class About extends React.Component {
                     <Modal.Header closeButton>
                         <Modal.Title>About this web app</Modal.Title>
                     </Modal.Header>
-                    <ModalBody className = "myform">
+                    <ModalBody className="myform">
+                        <div class="text_art">
+                            <span>
+                            <img src = {image} className = "about-image"></img>
+                            <div class="text_rotate">
+                                <h5 id="card_header">Fresh Furniture</h5>
+                            </div>
+                            </span>
+                        </div>
+                        <br/>
                         <h6>
-                        Fresh Furniture is a web application that aims to facilitate furniture trading on the market. It will support selling, buying, and even loaning second-hand or newly made furniture. Users have the option of opening their own store to sell their pre-owned furniture or service to build customized or fix furniture. Otherwise, users can just shop or order a service. Our app will aid in reducing waste and provide affordable furniture.
+                            Fresh Furniture is a web application that aims to facilitate furniture trading on the market. It will support selling, buying, and even loaning second-hand or newly made furniture. Users have the option of opening their own store to sell their pre-owned furniture or service to build customized or fix furniture. Otherwise, users can just shop or order a service. Our app will aid in reducing waste and provide affordable furniture.
                         </h6>
                     </ModalBody>
                     <Modal.Footer>
