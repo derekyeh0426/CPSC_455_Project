c2432608af8a8fbb1537114d3b2e256c078a76e4
Yukie Man <yukie.man97@gmail.com>
2021-07-07 22:51:23 -0700

    cleaned up code

--- frontend/pfiniture/src/components/my-account/Sidebar.js
+++ frontend/pfiniture/src/components/my-account/Sidebar.js
@@ -7,33 +7,26 @@ const Sidebar = () => {
                 <ul className="nav flex-column">
                     <li className="nav-item">
                         <a className="nav-link active" href="#">
-                                    <span style={{paddingRight: "6px"}}>
-                                        <i className="fas fa-home"></i>
-                                    </span>
-                            Dashboard <span className="sr-only">(current)</span>
+                            <span style={{paddingRight: "6px"}}><i className="fas fa-home"></i></span>
+                            Dashboard
+                            <span className="sr-only">(current)</span>
                         </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">
-                                    <span style={{paddingRight: "6px"}}>
-                                        <i className="fas fa-barcode"></i>
-                                    </span>
+                            <span style={{paddingRight: "6px"}}><i className="fas fa-barcode"></i></span>
                             Orders
                         </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">
-                                    <span style={{paddingRight: "6px"}}>
-                                        <i className="fas fa-shopping-cart"></i>
-                                    </span>
+                            <span style={{paddingRight: "6px"}}><i className="fas fa-shopping-cart"></i></span>
                             Cart
                         </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">
-                                    <span style={{paddingRight: "6px"}}>
-                                        <i className="fas fa-couch"></i>
-                                    </span>
+                            <span style={{paddingRight: "6px"}}><i className="fas fa-couch"></i></span>
                             My Furnitures
                         </a>
                     </li>
