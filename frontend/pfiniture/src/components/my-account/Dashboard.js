b431a5608c842b39dd5fb629da08c06208ddc418
Yukie <yukie.man97@gmail.com>
2021-07-15 04:59:32 -0700

    my furnitures now displayed on dashboard

--- frontend/pfiniture/src/components/my-account/Dashboard.js
+++ frontend/pfiniture/src/components/my-account/Dashboard.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Table from './Table'
+import DisplayMyFurnitures from './DisplayMyFurnitures'
+import {
+    Container
+} from '@material-ui/core'
 
 class Dashboard extends React.Component {
 
     render() {
         return (
             <div>
-                <h2>
-                    My Orders
-                </h2>
-                <Table/>
+                <h2>My Orders</h2>
+                <Table />
+                <br/>
+                <h2>My Furnitures</h2>
+                <div className="grid-container">
+                    <Container>
+                        <DisplayMyFurnitures />
+                    </Container>
+                </div>
             </div>
         )
     }
--- frontend/pfiniture/src/components/my-account/MyFurnitures.js
+++ frontend/pfiniture/src/components/my-account/MyFurnitures.js
@@ -1,29 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import client from '../../API/api'
 import { makeStyles } from '@material-ui/core/styles';
 import {
-    Grid,
-    Card,
-    Typography,
-    CardActions,
     Button,
     Container
 } from '@material-ui/core'
+import DisplayMyFurnitures from './DisplayMyFurnitures';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    cardRoot: {
-        width: 400,
-        maxHeight: 600
-    },
-    media: {
-        height: 300,
-    },
-    cardButtons: {
-        justifyContent: 'center'
-    },
     addFurnitureButton: {
         float: 'left',
         margin: 5
@@ -32,77 +16,12 @@ const useStyles = makeStyles((theme) => ({
 
 function MyFurnitures() {
     const classes = useStyles()
-    const [furnitures, setFurnitures] = useState('');
-
-    useEffect(() => {
-        client.furniture.getAllFurnitures().then(furnitures => {
-            setFurnitures(furnitures.data);
-        })
-    }, [])
-
-    const handleDelete = (e, id) => {
-        e.preventDefault();
-        client.furniture.deleteFurnitureById(id).then(() => {
-            client.furniture.getAllFurnitures().then(furnitures => {
-                setFurnitures(furnitures.data);
-            })
-        })
-    }
 
     return (
         <div className="grid-container">
             <Container>
-                {console.log(furnitures)}
                 <Button className={classes.addFurnitureButton} variant="outlined" color="primary">Add Furniture</Button>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center">
-                    {furnitures.length === 0
-                        ? ""
-                        : furnitures.map((furniture, index) => (
-                            <div key={index} className="furniture-spacing">
-                                <Card className={classes.cardRoot}>
-                                    {/* <CardActionArea>
-                                        <CardMedia
-                                            className={classes.media}
-                                            image={furniture.picture}
-                                            title={furniture.name}
-                                        />
-                                    </CardActionArea> */}
-                                    <Typography gutterBottom variant="h6" component="h2">
-                                        ${furniture.price} • {furniture.name}
-                                    </Typography>
-                                    {/* <IconButton
-                                        // TODO all cards expand when only want one card to expand
-                                        className={clsx(classes.expand, {
-                                            [classes.expandOpen]: expanded,
-                                        })}
-                                        onClick={handleExpandClick}
-                                        aria-expanded={expanded}
-                                        aria-label="show more">
-                                        <ExpandMoreIcon />
-                                    </IconButton> */}
-                                    <CardActions className={classes.cardButtons}>
-                                        <Button size="small" color="primary">
-                                            List for Sale
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Edit
-                                        </Button>
-                                        <Button
-                                            size="small"
-                                            color="secondary"
-                                            onClick={(e) => { handleDelete(e, furniture.id) }}>
-                                            Delete
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </div>
-                        ))
-                    }
-                </Grid>
+                <DisplayMyFurnitures />
             </Container>
         </div>
     )
