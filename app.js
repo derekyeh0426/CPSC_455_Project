9e4d07e5ca4dbe1feb006826096a4f7d0548f420
peterko <tyko@alumni.ubc.ca>
2021-07-27 14:48:00 -0700

    Fix app.js for production

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
@@ -13,19 +14,23 @@ const userRouter = require('./controllers/user');
 const imageRouter = require('./controllers/image');
 const listingRouter = require('./controllers/listing');
 
-
-
 mongoose
     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => console.log('connected to MongoDB'))
     .catch((error) => console.log('error connecting to MongoDB:', error.message));
-
 
 app.use('/api/v1/furnitures', furnitureRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/images', imageRouter);
 app.use('/api/v1/listings', listingRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 module.exports = app;
