f488f9b1b042a01f557e577943ee5a0e7764ebe6
peterko <tyko@alumni.ubc.ca>
2021-07-27 15:18:54 -0700

    Fix api endpoints

--- frontend/pfiniture/src/API/user-api.js
+++ frontend/pfiniture/src/API/user-api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { baseUrl} from "../constants";
-
 
 export default class userClient {
 
     static async getAllUsers() {
         try {
-            const response = axios.get(baseUrl + "/api/v1/users");
+            const response = axios.get("/api/v1/users");
             return response
         } catch (e) {
             console.log(e.message())
@@ -15,7 +13,7 @@ export default class userClient {
 
     static async deleteAllUsers() {
         try {
-            const response = axios.delete(baseUrl + `/api/v1/users/`);
+            const response = axios.delete(`/api/v1/users/`);
             return response
         } catch (e) {
             console.log(e.message())
@@ -24,7 +22,7 @@ export default class userClient {
 
     static async addUsers({name, email, token}) {
         try {
-            const response = axios.post(baseUrl + `/api/v1/users/`, {name, email, token});
+            const response = axios.post(`/api/v1/users/`, {name, email, token});
             return response
         } catch (e) {
             console.log(e.message())
@@ -33,7 +31,7 @@ export default class userClient {
 
     static async deleteUserById(id) {
         try {
-            const response = axios.delete(baseUrl + `/api/v1/users/${id}`);
+            const response = axios.delete(`/api/v1/users/${id}`);
             return response
         } catch (e) {
             console.log(e.message())
@@ -42,7 +40,7 @@ export default class userClient {
 
     static async getUserById(id) {
         try {
-            const response = axios.get(baseUrl + `/api/v1/users/${id}`);
+            const response = axios.get(`/api/v1/users/${id}`);
             return response
         } catch (e) {
             console.log(e.message())
