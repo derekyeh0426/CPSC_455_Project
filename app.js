6121783bcec2c4f20172bb249b4f10ca62bb564c
peterko <tyko@alumni.ubc.ca>
2021-07-27 15:12:57 -0700

    Fix CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(cors());
+app.options('*', cors());
 
 const furnitureRouter = require('./controllers/furniture');
 const userRouter = require('./controllers/user');
