31a8d816a0dc5068c1e48a6f317e00bbfe14c27c
dan <dhyunlee1008@gmail.com>
2021-07-28 17:27:03 -0700

    fix bucket permission to access images with public url and implement delete

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
-
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
