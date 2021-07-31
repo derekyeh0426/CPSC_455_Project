a2165642e5b7ac323c407cb8e8851d4b98b6bbed
Yukie <yukie.man97@gmail.com>
2021-07-31 06:49:16 -0700

    without GoogleId.js

--- frontend/pfiniture/src/components/login/GoogleLogIn.js
+++ frontend/pfiniture/src/components/login/GoogleLogIn.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux'
 import client from '../../API/api'
 import './LogInForm.css'
 import { refreshTokenSetup } from '../../utility';
-import { GOOGLE_CLIENT_ID } from '../../GoogleId'
+// import { GOOGLE_CLIENT_ID } from '../../GoogleId'
 
 class GoogleLogIn extends React.Component {
     constructor(props) {
@@ -24,7 +24,8 @@ class GoogleLogIn extends React.Component {
     }
 
     componentDidMount(){
-        console.log(GOOGLE_CLIENT_ID);
+        // console.log(GOOGLE_CLIENT_ID);
+        console.log(process.env.GOOGLE_CLIENT_ID);
         console.log(this.props.name);
         console.log(this.props.email);
     }
@@ -71,7 +72,8 @@ class GoogleLogIn extends React.Component {
             <div>
                 {this.state.isLogined ?
                     <GoogleLogout
-                        clientId= {GOOGLE_CLIENT_ID}
+                        // clientId= {GOOGLE_CLIENT_ID}
+                        clientId= {process.env.GOOGLE_CLIENT_ID}
                         buttonText="Logout"
                         onLogoutSuccess={this.handleSuccessfulLogOut}
                         onFailure={this.handleLogoutFailure}
@@ -79,7 +81,8 @@ class GoogleLogIn extends React.Component {
                     </GoogleLogout>
                     :
                     <GoogleLogin className="google-login-button"
-                        clientId= {GOOGLE_CLIENT_ID}
+                        // clientId= {GOOGLE_CLIENT_ID}    
+                        clientId= {process.env.GOOGLE_CLIENT_ID}
                         buttonText="Log in with Google!"
                         onSuccess={this.handleSuccessfulLogIn}
                         onFailure={this.handleLoginFailure}
