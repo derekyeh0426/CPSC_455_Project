de23b5b1f9907f8ba2da994d6898dc8726e5d7d6
hsinhao <c8e65e7d1a9f9b38a380d108d8ccf5b8c7a69e9e>
2021-08-09 20:44:02 -0700

    fix add to cart when use is log out

--- frontend/pfiniture/src/components/landing/DisplayIndividualFurniture.js
+++ frontend/pfiniture/src/components/landing/DisplayIndividualFurniture.js
@@ -198,9 +198,12 @@ function DisplayIndividualFurniture(props) {
                                 </IconButton>
                                 <CardActions>
                                     <ViewSellerProfile userInfo={listing.user} />
+                                    {props.isLogIn?
                                     <Button size="small" color="primary" onClick={() => onAddToCart(listing.id)}>
                                         Add to Cart
                                     </Button>
+                                    : ""
+                                    }
                                 </CardActions>
                             </Card>
                         </div>
