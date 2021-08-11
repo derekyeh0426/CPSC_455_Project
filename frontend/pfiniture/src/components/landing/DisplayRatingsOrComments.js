30a62f26842fa8dc003c75b72f21b92f9269df6d
Yukie <yukie.man97@gmail.com>
2021-08-11 03:36:27 -0700

    cleaned code and made DisplayRatingsOrComments file

---
+++ frontend/pfiniture/src/components/landing/DisplayRatingsOrComments.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Grid,
+    Typography,
+    Paper,
+    Button
+} from '@material-ui/core';
+import client from "../../API/api";
+import { RATINGS } from '../../constants';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(1),
+        flexGrow: 1,
+    },
+    gridPaper: {
+        padding: theme.spacing(2),
+        margin: 'auto',
+        maxWidth: 500,
+    }
+}));
+
+export default function DisplayRatingsOrComments(props) {
+    const classes = useStyles();
+    const [isComments, setIsComments] = React.useState(props.isComments)
+    const [isRatings, setIsRatings] = React.useState(props.isRatings)
+    const [ratingsOrComments, setRatingsOrComments] = React.useState(props.ratingsOrComments);
+
+    useEffect(() => {
+        setIsComments(props.isComments);
+        setIsRatings(props.isRatings);
+        setRatingsOrComments(props.ratingsOrComments);
+    }, [props.isRatings, props.isComments, props.ratingsOrComments])
+
+    return (
+        <div>
+            {!ratingsOrComments
+                ? console.log(ratingsOrComments)
+                : ratingsOrComments.map((ratingOrComment, index) => {
+                    return (
+                        <div key={index} className={classes.root}>
+                            <Paper className={classes.gridPaper}>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    direction="row"
+                                    justifyContent="center"
+                                    alignItems="center">
+                                    <Grid item xs={12} sm container>
+                                        <Grid item xs container direction="column" spacing={2}>
+                                            <Grid item xs>
+                                                <Typography gutterBottom variant="h6" component="h2">
+                                                    {isComments && isRatings
+                                                        ? ratingOrComment.name
+                                                        : ratingOrComment.user.name}
+                                                </Typography>
+                                                {isComments ?
+                                                    <Typography variant="body2" gutterBottom>
+                                                        {ratingOrComment.comment}
+                                                    </Typography>
+                                                    : ""}
+                                                {isRatings
+                                                    ? <Typography variant="body2" color="textSecondary">
+                                                        Rating: {ratingOrComment.rating}
+                                                    </Typography>
+                                                    : ""
+                                                }
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- frontend/pfiniture/src/components/landing/SellerReviews.js
+++ frontend/pfiniture/src/components/landing/SellerReviews.js
@@ -4,10 +4,8 @@ import {
     Grid,
     Typography,
     Paper,
-    Button
 } from '@material-ui/core';
-import client from "../../API/api";
-import { RATINGS } from '../../constants';
+import DisplayRatingsOrComments from './DisplayRatingsOrComments'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -48,147 +46,74 @@ export default function SellerReviews(props) {
     }, [props.userInfo, props.ratings, props.comments, props.isOpen])
 
     function getReviews() {
-    if (isOpen) {
-        let tempReviewArray = [];
-        let tempUpdatedComments =  JSON.parse(JSON.stringify(comments));
-        let tempUpdatedRatings = JSON.parse(JSON.stringify(ratings));
-        if (!comments && !ratings) {
-            console.log("comments and ratings are empty")
-        } else {
-            console.log("comments and ratings are here")
-            comments.forEach(comment => {
-                ratings.forEach(rating => {
-                    if (comment.user.id === rating.user.id) {
-                        tempReviewArray.push({
-                            "id": comment.user.id,
-                            "name": comment.user.name,
-                            "comment": comment.comment,
-                            "rating": rating.rating
-                        });
-                    }
+        if (isOpen) {
+            let tempReviewArray = [];
+            let tempUpdatedComments = JSON.parse(JSON.stringify(comments));
+            let tempUpdatedRatings = JSON.parse(JSON.stringify(ratings));
+            if (!comments && !ratings) {
+                console.log("comments and ratings are empty")
+            } else {
+                console.log("comments and ratings are here")
+                comments.forEach(comment => {
+                    ratings.forEach(rating => {
+                        if (comment.user.id === rating.user.id) {
+                            tempReviewArray.push({
+                                "id": comment.user.id,
+                                "name": comment.user.name,
+                                "comment": comment.comment,
+                                "rating": rating.rating
+                            });
+                        }
+                    });
+                    setReviewArray(tempReviewArray);
+                    console.log(comments)
+                    console.log("----")
+                    console.log(tempReviewArray);
                 });
-                setReviewArray(tempReviewArray);
-                console.log(comments)
-                console.log("----")
-                console.log(tempReviewArray);
-            });
 
-            comments.forEach((comment, index) => {
-                tempReviewArray.forEach(review => {
-                    if (review.id === comment.user.id) {
-                        tempUpdatedComments.splice(index, 1);
-                    }
+                comments.forEach((comment, index) => {
+                    tempReviewArray.forEach(review => {
+                        if (review.id === comment.user.id) {
+                            tempUpdatedComments.splice(index, 1);
+                        }
+                    });
+                    setUpdatedComments(tempUpdatedComments);
                 });
-                setUpdatedComments(tempUpdatedComments);
-            });
 
-            ratings.forEach((rating, index) => {
-                tempReviewArray.forEach(review => {
-                    if (review.id === rating.user.id) {
-                        tempUpdatedRatings.splice(index, 1);
-                    }
+                ratings.forEach((rating, index) => {
+                    tempReviewArray.forEach(review => {
+                        if (review.id === rating.user.id) {
+                            tempUpdatedRatings.splice(index, 1);
+                        }
+                    });
+                    setUpdatedRatings(tempUpdatedRatings);
                 });
-                setUpdatedRatings(tempUpdatedRatings);
-            });
+            }
         }
     }
-}
 
     return (
         <div>
             {!reviewArray
                 ? "!reviewArray"
-                : reviewArray.map((review, index) => {
-                    return (
-                        <div key={index} className={classes.root}>
-                            <Paper className={classes.gridPaper}>
-                                <Grid
-                                    container
-                                    spacing={2}
-                                    direction="row"
-                                    justifyContent="center"
-                                    alignItems="center">
-                                    <Grid item xs={12} sm container>
-                                        <Grid item xs container direction="column" spacing={2}>
-                                            <Grid item xs>
-                                                <Typography gutterBottom variant="h6" component="h2">
-                                                    {review.name}
-                                                </Typography>
-                                                <Typography variant="body2" gutterBottom>
-                                                    {review.comment}
-                                                </Typography>
-                                                <Typography variant="body2" color="textSecondary">
-                                                    Rating: {review.rating}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
-                        </div>
-                    )
-                })
+                : <DisplayRatingsOrComments
+                    isRatings={true}
+                    isComments={true}
+                    ratingsOrComments={reviewArray} />
             }
             {!updatedComments
                 ? ""
-                :
-                updatedComments.map((comment, index) => {
-                    return (
-                        <div key={index} className={classes.root}>
-                            <Paper className={classes.gridPaper}>
-                                <Grid
-                                    container
-                                    spacing={2}
-                                    direction="row"
-                                    justifyContent="center"
-                                    alignItems="center">
-                                    <Grid item xs={12} sm container>
-                                        <Grid item xs container direction="column" spacing={2}>
-                                            <Grid item xs>
-                                                <Typography gutterBottom variant="h6" component="h2">
-                                                    {comment.user.name}
-                                                </Typography>
-                                                <Typography variant="body2" gutterBottom>
-                                                    {comment.comment}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
-                        </div>
-                    )
-                })
+                : <DisplayRatingsOrComments
+                    isRatings={false}
+                    isComments={true}
+                    ratingsOrComments={updatedComments} />
             }
             {!updatedRatings
                 ? ""
-                : updatedRatings.map((rating, index) => {
-                    return (
-                        <div key={index} className={classes.root}>
-                            <Paper className={classes.gridPaper}>
-                                <Grid
-                                    container
-                                    spacing={2}
-                                    direction="row"
-                                    justifyContent="center"
-                                    alignItems="center">
-                                    <Grid item xs={12} sm container>
-                                        <Grid item xs container direction="column" spacing={2}>
-                                            <Grid item xs>
-                                                <Typography gutterBottom variant="h6" component="h2">
-                                                    {rating.user.name}
-                                                </Typography>
-                                                <Typography variant="body2" color="textSecondary">
-                                                    Rating: {rating.rating}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
-                        </div>
-                    )
-                })
+                : <DisplayRatingsOrComments
+                    isRatings={true}
+                    isComments={false}
+                    ratingsOrComments={updatedRatings} />
             }
         </div>
     )
