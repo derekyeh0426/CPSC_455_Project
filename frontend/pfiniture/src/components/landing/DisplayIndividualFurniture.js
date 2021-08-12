32a89ba9277ee5e9a405c40ac8eac08da48089a6
Yukie <yukie.man97@gmail.com>
2021-08-12 02:01:39 -0700

    display remove or add item button depending on cart list

--- frontend/pfiniture/src/components/landing/DisplayIndividualFurniture.js
+++ frontend/pfiniture/src/components/landing/DisplayIndividualFurniture.js
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux'
 import client from "../../API/api";
-import {Grid,Card,CardActionArea,CardMedia,CardContent,Typography,CardActions,Button,
-    IconButton,Collapse,TextField,InputAdornment,FormControl,InputLabel,Select,MenuItem,Slider} from '@material-ui/core'
-import { onAddToCart } from '../../helpers'
+import {
+    Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button,
+    IconButton, Collapse, TextField, InputAdornment, FormControl, InputLabel, Select, MenuItem, Slider
+} from '@material-ui/core'
+import { onAddToCart, updateToCart } from '../../helpers'
 import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import ViewSellerProfile from "./ViewSellerProfile"
@@ -66,19 +68,30 @@ function DisplayIndividualFurniture(props) {
     const max = 1000;
     const classes = useStyles();
     const [buyerId, setBuyerId] = useState("");
+    const [cartId, setCartId] = useState("");
+    const [cartListings, setCartListings] = useState([]);
     const [expandedId, setExpandedId] = useState(-1);
     const [searchTerm, setSearchTerm] = useState("");
     const [typeTerm, setTypeTerm] = useState("all");
     const [listings, setListings] = useState([]);
     const [originalListings, setOriginalListings] = useState([]);
     const [priceRange, setPriceRange] = useState([min, max]);
-    const [location, setLocation] = useState("all")
+    const [location, setLocation] = useState("all");
 
     React.useEffect(() => {
         setBuyerId(store.getState().id)
         client.listing.getAllListingsDescendingOrder().then(listings => {
             setListings(listings.data);
             setOriginalListings(listings.data);
+            client.user.getUserById(store.getState().id).then(buyerInfo => {
+                const cart = buyerInfo.data.cart;
+                if (!cart) {
+                    setCartListings([]);
+                } else {
+                    setCartId(cart.id)
+                    setCartListings(cart.listings);
+                }
+            })
         })
     }, [store.getState().id]);
 
@@ -140,93 +153,93 @@ function DisplayIndividualFurniture(props) {
         <div>
             <div className={classes.alignCenter}>
                 <div>
-                <TextField
-                    className={classes.searchMargin}
-                    placeholder="Search..."
-                    id="input-with-icon-textfield"
-                    onChange={(event) => { setSearchTerm(event.target.value) }}
-                    value = {searchTerm}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="demo-simple-select-label">Furniture Type</InputLabel>
-                    <Select
-                        labelId="select-furniture-type-label"
-                        id="select-furniture-type"
-                        value={typeTerm}
-                        onChange={(event) => { setTypeTerm(event.target.value) }}
-                        displayEmpty
-                        className={classes.selectEmpty}
-                    >
-                        <MenuItem value="all">
-                            <em>All</em>
-                        </MenuItem>
-                        {FURNITURE_TYPES.map((type) => {
-                            return <MenuItem
-                                key={type}
-                                value={type}
-                                default=''>
-                                {type}
+                    <TextField
+                        className={classes.searchMargin}
+                        placeholder="Search..."
+                        id="input-with-icon-textfield"
+                        onChange={(event) => { setSearchTerm(event.target.value) }}
+                        value={searchTerm}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Furniture Type</InputLabel>
+                        <Select
+                            labelId="select-furniture-type-label"
+                            id="select-furniture-type"
+                            value={typeTerm}
+                            onChange={(event) => { setTypeTerm(event.target.value) }}
+                            displayEmpty
+                            className={classes.selectEmpty}
+                        >
+                            <MenuItem value="all">
+                                <em>All</em>
                             </MenuItem>
-                        })}
-                    </Select>
-                </FormControl>
+                            {FURNITURE_TYPES.map((type) => {
+                                return <MenuItem
+                                    key={type}
+                                    value={type}
+                                    default=''>
+                                    {type}
+                                </MenuItem>
+                            })}
+                        </Select>
+                    </FormControl>
 
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="select-location-filter">Select Location</InputLabel>
-                    <Select
-                        labelId="select-location-label"
-                        id="select-location"
-                        value={location}
-                        onChange={(event) => { setLocation(event.target.value) }}
-                        displayEmpty
-                        className={classes.selectEmpty}
-                    >
-                        <MenuItem value="all">
-                            <em>All</em>
-                        </MenuItem>
-                        {LOCATIONS.map((location) => {
-                            return <MenuItem
-                                key={location}
-                                value={location}
-                                default=''>
-                                {location}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="select-location-filter">Select Location</InputLabel>
+                        <Select
+                            labelId="select-location-label"
+                            id="select-location"
+                            value={location}
+                            onChange={(event) => { setLocation(event.target.value) }}
+                            displayEmpty
+                            className={classes.selectEmpty}
+                        >
+                            <MenuItem value="all">
+                                <em>All</em>
                             </MenuItem>
-                        })}
-                    </Select>
-                </FormControl>
+                            {LOCATIONS.map((location) => {
+                                return <MenuItem
+                                    key={location}
+                                    value={location}
+                                    default=''>
+                                    {location}
+                                </MenuItem>
+                            })}
+                        </Select>
+                    </FormControl>
                 </div>
-                    
+
                 <div>
                     <Button color="primary" className={classes.buttonMargin} onClick={handleSearch}>Search</Button>
                     <Button color="primary" onClick={resetSearch}>Clear Filters</Button>
                 </div>
             </div>
             <div className={classes.root}>
-                    <Typography id="range-slider" gutterBottom>
-                        Price Range • ${priceRange[0]} - ${priceRange[1]}
-                    </Typography>
-                    <Slider
-                        value={priceRange}
-                        onChange={handleChange}
-                        valueLabelDisplay="auto"
-                        aria-labelledby="range-slider"
-                        getAriaValueText={priceRangeText}
-                        step={100}
-                        marks
-                        min={min}
-                        max={max}
-                    />
-                </div>
+                <Typography id="range-slider" gutterBottom>
+                    Price Range • ${priceRange[0]} - ${priceRange[1]}
+                </Typography>
+                <Slider
+                    value={priceRange}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={priceRangeText}
+                    step={100}
+                    marks
+                    min={min}
+                    max={max}
+                />
+            </div>
             <div className={classes.listingsSection}>
                 {listings.length === 0
-                    ? 
+                    ?
                     <div>
                         <p>No Matched Results</p>
                         <p>Clear the filter to search again</p>
@@ -238,49 +251,92 @@ function DisplayIndividualFurniture(props) {
                         justifyContent="center"
                         alignItems="center">
                         {listings.map((listing, index) => (
-                                <div key={index} className="furniture-spacing">
-                                    <Card key={index} className={classes.cardRoot}>
-                                        <CardActionArea>
-                                            <CardMedia
-                                                className={classes.media}
-                                                image={listing.images[0] ? listing.images[0].imageUrl : ""}
-                                                title={listing.furniture.name}
-                                            />
-                                        </CardActionArea>
-                                        <Typography gutterBottom variant="h6" component="h2">
-                                            ${listing.furniture.price} • {listing.furniture.name}
-                                        </Typography>
-                                        <Collapse in={expandedId === index} timeout="auto" unmountOnExit>
-                                            <CardContent>
-                                                <Typography variant="body2" color="textSecondary" component="p">
-                                                    Seller: {listing.user.name} ({listing.user.rating})
-                                                </Typography>
-                                                <Typography variant="body2" color="textSecondary" component="p">
-                                                    Type: {listing.type}
-                                                </Typography>
-                                                <Typography variant="body2" color="textSecondary" component="p">
-                                                    Description: {listing.description}
-                                                </Typography>
-                                            </CardContent>
-                                        </Collapse>
-                                        <IconButton
-                                            onClick={() => { handleExpandClick(index) }}
-                                            aria-expanded={expandedId === index}
-                                            aria-label="show more">
-                                            <ExpandMoreIcon />
-                                        </IconButton>
-                                        <CardActions>
-                                            <ViewSellerProfile userInfo={listing.user} />
-                                            {props.isLogIn ?
-                                                <Button size="small" color="primary" onClick={() => onAddToCart(listing.id, buyerId)}>
-                                                    Add to Cart
-                                                </Button>
-                                                : ""
-                                            }
-                                        </CardActions>
-                                    </Card>
-                                </div>
-                            ))}
+                            <div key={index} className="furniture-spacing">
+                                <Card key={index} className={classes.cardRoot}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image={listing.images[0] ? listing.images[0].imageUrl : ""}
+                                            title={listing.furniture.name}
+                                        />
+                                    </CardActionArea>
+                                    <Typography gutterBottom variant="h6" component="h2">
+                                        ${listing.furniture.price} • {listing.furniture.name}
+                                    </Typography>
+                                    <Collapse in={expandedId === index} timeout="auto" unmountOnExit>
+                                        <CardContent>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                Seller: {listing.user.name} ({listing.user.rating})
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                Type: {listing.type}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                Description: {listing.description}
+                                            </Typography>
+                                        </CardContent>
+                                    </Collapse>
+                                    <IconButton
+                                        onClick={() => { handleExpandClick(index) }}
+                                        aria-expanded={expandedId === index}
+                                        aria-label="show more">
+                                        <ExpandMoreIcon />
+                                    </IconButton>
+                                    <CardActions>
+                                        <ViewSellerProfile userInfo={listing.user} />
+                                        {props.isLogIn
+                                            ?
+                                            <div>
+                                                {!cartListings
+                                                    ?
+                                                    <Button size="small" color="primary"
+                                                        onClick={() => onAddToCart(listing.id, buyerId)}>
+                                                        Add to Cart
+                                                    </Button>
+                                                    :
+                                                    <div>
+                                                        {cartListings.includes(listing.id)
+                                                            ? <Button
+                                                                size="small"
+                                                                color="secondary"
+                                                                onClick={() => updateToCart(listing.id, listings, cartId)}
+                                                            > Remove From Cart
+                                                            </Button>
+                                                            :
+                                                            <Button
+                                                                size="small" color="primary"
+                                                                onClick={() => onAddToCart(listing.id, buyerId)}>
+                                                                Add to Cart
+                                                            </Button>
+                                                        }
+                                                    </div>
+                                                    // cartListings.map((cartListing) => {
+                                                    //     if (cartListing === listing.id) {
+                                                    //         console.log("true")
+                                                    //         return <Button
+                                                    //             size="small"
+                                                    //             color="secondary"
+                                                    //             onClick={() => updateToCart(listing.id, listings, cartId)}
+                                                    //         >
+                                                    //             Remove from Cart
+                                                    //         </Button>
+                                                    //     } else {
+                                                    //         console.log("false")
+                                                    //         return <Button
+                                                    //             size="small" color="primary"
+                                                    //             onClick={() => onAddToCart(listing.id, buyerId)}>
+                                                    //             Add to Cart
+                                                    //         </Button>
+                                                    //     }
+                                                    // })
+                                                }
+                                            </div>
+                                            : ""
+                                        }
+                                    </CardActions>
+                                </Card>
+                            </div>
+                        ))}
                     </Grid>
                 }
             </div>
