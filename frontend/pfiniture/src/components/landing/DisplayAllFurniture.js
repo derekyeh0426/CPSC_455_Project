e44ca228879fd2b27e9e6aa61ecf826745d05069
hsinhao <c8e65e7d1a9f9b38a380d108d8ccf5b8c7a69e9e>
2021-08-11 23:13:29 -0700

    fix

--- frontend/pfiniture/src/components/landing/DisplayAllFurniture.js
+++ frontend/pfiniture/src/components/landing/DisplayAllFurniture.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './DisplayAllFurniture.css';
-import {Container} from '@material-ui/core'
+import {Container,IconButton} from '@material-ui/core'
 import DisplayIndividualFurniture from "./DisplayIndividualFurniture"
 
 
 export default function DisplayAllFurniture() {
-
     return (
         <div>
             <div className="grid-container">
